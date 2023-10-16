4d877a6bd601b51efa6772b29fe63af4b98feaf2
Brinkley Hill <8brink@gmail.com>
2023-10-16 13:24:20 -0400

    update fav, layout, footer, main, settings, header

---
+++ app/components/header/header.jsx
@@ -0,0 +1,3 @@
+export default function Header() {
+ return <header className="min-h-[10vh]">This is the header</header>;
+}
---
+++ app/settings/page.jsx
@@ -0,0 +1,7 @@
+export default function Settings() {
+ return (
+  <main className="min-h-[80vh] bg-blue-900">
+   <h2>Settings Page</h2>
+  </main>
+ );
+}
