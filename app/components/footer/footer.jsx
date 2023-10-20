0253962db48dc62cfa5f54d897bd03eb3a81b85f
Brinkley Hill <8brink@gmail.com>
2023-10-20 14:40:05 -0400

    updates

--- app/components/footer/footer.jsx
+++ app/components/footer/footer.jsx
@@ -1,32 +1,32 @@
 import Link from "next/link";
 
 export default function Footer() {
-  return (
-    <footer className="min-h-[10vh] flex justify-between px-7">
-      {/* insert clever waffle pun here
+ return (
+  <footer className="min-h-[10vh] flex justify-between px-3 sm:px-7 border-t">
+   {/* insert clever waffle pun here
       WHY ARE WEATHER WAFFLE PUNS SO HARD*/}
-      <div className="flex flex-col justify-center text-s font-bold italic">
-        waffle waffle waffle waffle waffle
-      </div>
+   <div className="flex flex-col justify-center text-xs sm:text-base font-bold italic hover:text-orange-400 transition">
+    <Link href={"https://www.youtube.com/watch?v=eDU0CTDMk2g&pp=ygULd2FmZmxlIHNvbmc%3D"} target="_blank">
+     waffle waffle waffle
+    </Link>
+   </div>
 
-      <div className="flex items-center">
-        <Link
-          href={
-            "https://www.youtube.com/watch?v=eDU0CTDMk2g&pp=ygULd2FmZmxlIHNvbmc%3D"
-          }
-          target="_blank"
-        >
-          {/*WHY DO THE SVGS NOT WORK FOR ME WHAT AM I DOING WRONG BROOOOOOO
-          I know it goes here haha*/}
-        </Link>
-      </div>
-
-      {/* wanted to add links to each of our names but could NOT get it to work LOL
+   {/* wanted to add links to each of our names but could NOT get it to work LOL
     i.e. "Brinkley" would be a link to your LinkedIn or github or your portfolio or something
     and same for my name */}
-      <div className="flex flex-col justify-center text-xs font-bold">
-        Made by Brinkley and Michael
-      </div>
-    </footer>
-  );
+
+   <div className="flex justify-center items-center text-xs sm:text-base ">
+    <span>
+     Made by{" "}
+     <Link href={"https://www.linkedin.com/in/michael-mason-3782171b9/"} className="hover:underline">
+      Michael
+     </Link>{" "}
+     and{" "}
+     <Link href={"https://github.com/brinkhill"} className="hover:underline">
+      Brinkley
+     </Link>
+    </span>
+   </div>
+  </footer>
+ );
 }
